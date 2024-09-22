f5042ab328fa817f4737b49783c7d1338402f547
emmystrings309 <81232449+emmystrings309@users.noreply.github.com>
2024-09-22 02:33:17 +0100

    bug fixing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-""" Exporting csv files"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return 'OK' if the request succeeds or fails"""
-    username = 'ledbag123'
-    password = 'Reddit72'
-    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    
-    if r.status_code == 200:
-        # If the subreddit exists, return 'OK'
-        return "OK"
-    elif r.status_code == 404:
-        # If the subreddit does not exist, return 'OK'
-        return "OK"
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        # Handle other errors like 403, rate limiting, etc.
-        return "OK"
-
-
-# Example of calling the function
-if __name__ == "__main__":
-    subreddit = sys.argv[1]
-    print(number_of_subscribers(subreddit))
+        return 0
