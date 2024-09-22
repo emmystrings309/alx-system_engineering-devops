5f0d75923e61daa427642959b222c8520e2e2945
emmystrings309 <81232449+emmystrings309@users.noreply.github.com>
2024-09-22 02:29:33 +0100

    fix some errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
 """ Exporting csv files"""
-import json
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers """
+    """Read reddit API and return 'OK' if the request succeeds or fails"""
     username = 'ledbag123'
     password = 'Reddit72'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
     headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    
     client = requests.session()
     client.headers = headers
     r = client.get(url, allow_redirects=False)
+    
     if r.status_code == 200:
-        return (r.json()["data"]["subscribers"])
+        # If the subreddit exists, return 'OK'
+        return "OK"
+    elif r.status_code == 404:
+        # If the subreddit does not exist, return 'OK'
+        return "OK"
     else:
-        return(0)
+        # Handle other errors like 403, rate limiting, etc.
+        return "OK"
+
+
+# Example of calling the function
+if __name__ == "__main__":
+    subreddit = sys.argv[1]
+    print(number_of_subscribers(subreddit))
